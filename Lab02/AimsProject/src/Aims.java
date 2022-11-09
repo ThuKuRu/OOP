0caeda7120c7b1bf3896f0335b9a4f3bcddbe9b6
ThuKuRu <Thu2552002bn@gmail.com>
2022-11-09 11:42:35 +0700

    Lab02

--- Lab02/AimsProject/src/Aims.java
+++ Lab02/AimsProject/src/Aims.java
@@ -16,25 +16,17 @@ public class Aims {
 
        DigitalVideoDisc dvd3 =  new DigitalVideoDisc("Aladin", "Animation", 18.99f);
         anOder.addDigitalVideoDisc(dvd3);
-        DigitalVideoDisc[] dvds = {dvd1, dvd2, dvd3};
 
         System.out.println(" Total Cost is: ");
-        System.out.println(anOder.totalCost(dvds));
+        System.out.println(anOder.totalCost());
 
         System.out.println(" danh sách trước khi xóa: ");
-        for (DigitalVideoDisc dvd : dvds){
-            System.out.println(dvd);
-        }
-        anOder.removeDigitalVideoDisc(dvds, dvd2);
+        anOder.printDigitalVideoDisc();
+
+        anOder.removeDigitalVideoDisc(dvd2);
         System.out.println(" Danh sách sau khi xóa");
-        for (DigitalVideoDisc dvd : dvds){
-            System.out.println(dvd);
-        }
-
-        
+        anOder.printDigitalVideoDisc();
     }
-
-
 
 
 }
--- Lab02/AimsProject/src/Cart.java
+++ Lab02/AimsProject/src/Cart.java
@@ -4,37 +4,52 @@ import java.util.Scanner;
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
 
-    private DigitalVideoDisc itemsOrdered[] =
+    private  DigitalVideoDisc[] itemsOrdered =
             new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
 
-    private static int qtyOrdered = 0;
+    private int qtyOrdered = 0;
 
-    public static void addDigitalVideoDisc(DigitalVideoDisc dvd){
-        qtyOrdered++;
+    public DigitalVideoDisc[] getItemsOrdered() {
+        return itemsOrdered;
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd){
+        itemsOrdered[this.qtyOrdered] = dvd;
+        this.qtyOrdered++;
         if(qtyOrdered >= MAX_NUMBERS_ORDERED){
             System.out.println(" Giỏ hàng gần đầy");
         }else{
             System.out.println(" Đĩa đã được thêm");
         }
-
     }
 
-    public static void removeDigitalVideoDisc(DigitalVideoDisc[] dvds, DigitalVideoDisc dvd){
-        for(int i = 0; i < dvds.length; i++){
-            if(dvds[i].equals(dvd)){
+    public void removeDigitalVideoDisc(DigitalVideoDisc dvd){
+        int remove = 0;
+        for(int i = 0; i < qtyOrdered; i++){
+            if(itemsOrdered[i].equals(dvd)){
                 System.out.println(" Xóa bỏ thành công");
-                break;
+                remove = i;
             }
         }
+        qtyOrdered = qtyOrdered -1;
+        for( int i = remove; i < qtyOrdered; i++){
+            itemsOrdered[i] = itemsOrdered[i + 1];
+        }
 
     }
 
-    public static float totalCost(DigitalVideoDisc[] dvds){
+    public float totalCost(){
         float total = 0;
-        for(int i = 0; i < dvds.length; i++){
-            total += dvds[i].getCost();
+        for(int i = 0; i < qtyOrdered; i++){
+            total += itemsOrdered[i].getCost();
         }
         return total;
+    }
+
+    public void printDigitalVideoDisc(){
+        for (int i = 0; i < qtyOrdered; i++){
+            System.out.println(itemsOrdered[i]);
+        }
     }
 
 
